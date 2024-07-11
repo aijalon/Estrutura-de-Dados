016f8a42afffc819bd32a0eb4773ac999940ab0a
Aijalon Júnior <juniorsjs6@hotmail.com>
2024-07-10 21:08:06 -0300

    Aula 10.07.2024

--- Restaurante/src/restaurante/Cardapio.java
+++ Restaurante/src/restaurante/Cardapio.java
@@ -18,19 +18,37 @@ public class Cardapio {
 
 	void removerItem(int indice) {
 		// TODO implementar exclusão de item do cardápio da posição informada
+		if(indice<0 || indice>this.itens.length-1) {
+			System.out.println("Valor incorreto, tente novamente.");
+			return;
+		}else if(this.itens.length==0) {
+			System.out.println("Não existe itens adicionado.");
+			return;
+		}else {
+			ItemCardapio[] itens = new ItemCardapio[this.itens.length-1];
+			
+			for(int i = indice; i < this.itens.length; i++) {
+			if(i+1==this.itens.length) {
+				break;
+			}else {
+				this.itens[i] = this.itens[i+1];
+			}			
+			}
+			
+			for(int i = 0; i <itens.length; i++) {
+				itens[i] = this.itens[i];
+			}
+			
+			this.itens = itens;
+		}
 	}
 
 	void imprimirItensCardapio(double precoMinimo, double precoMaximo) {
-		// TODO implementar código para imprimir na console os itens
-		//  do cardápio que estiverem entre o preço mínimo e máximo
 		for (int i =0; i<this.itens.length; i++) {
 			if(this.itens[i].possuiValorEntre(precoMinimo, 
 					precoMaximo)) {
 				this.itens[i].imprimir();
 			}
 		}
-		
-		
-		
 	}
 }
