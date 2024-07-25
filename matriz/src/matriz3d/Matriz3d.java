c4778dd6a423524f25f0b7106749dbfa322013d9
Aijalon Júnior <juniorsjs6@hotmail.com>
2024-07-24 21:08:39 -0300

    Aula 24.07

---
+++ matriz/src/matriz3d/Matriz3d.java
@@ -0,0 +1,11 @@
+package matriz3d;
+
+import java.util.ArrayList;
+
+public class Matriz3d {
+
+	int[][][] matriz = new int[2][2][2];
+	
+	ArrayList<ArrayList<ArrayList<Integer>>> matrizAl =  new ArrayList<>();
+	
+}
