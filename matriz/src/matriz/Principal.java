552f473742dc2dce76ac98128db48ae647cf7b35
Aijalon Júnior <juniorsjs6@hotmail.com>
2024-07-31 22:06:36 -0300

    Revisao

--- matriz/src/matriz/Principal.java
+++ matriz/src/matriz/Principal.java
@@ -8,24 +8,29 @@ public class Principal {
 
 		Matriz matriz = new Matriz(3, 3);
 		
-		try {
-			matriz.adicionarElemento("a");
-			matriz.adicionarElemento("b");
-			matriz.adicionarElemento("c");
-			matriz.adicionarElemento("d");
-			
-			matriz.removerElemento();
-			matriz.removerElemento();
-			matriz.removerElemento();
-			
-			matriz.adicionaCapacidade(1, 2);
-			
-			System.out.println(matriz.buscarElemento("b"));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		int[][] matriz2 = new int[10][10];
+		
+		/*
+		 * for(int i = 0; i<10; i++) { for(int j = 0; j<10; j++) {
+		 * System.out.println(Math.pow((i+1)+(j+1), 2)); } }
+		 */
+		
+		System.out.println((1+1));
 		
 		
-		System.out.println(Arrays.deepToString(matriz.matriz));
+		/*
+		 * try { matriz.adicionarElemento("a"); matriz.adicionarElemento("b");
+		 * matriz.adicionarElemento("c"); matriz.adicionarElemento("d");
+		 * 
+		 * matriz.removerElemento(); matriz.removerElemento(); matriz.removerElemento();
+		 * 
+		 * matriz.adicionaCapacidade(1, 2);
+		 * 
+		 * System.out.println(matriz.buscarElemento("b")); } catch (Exception e) {
+		 * e.printStackTrace(); }
+		 * 
+		 * 
+		 * System.out.println(Arrays.deepToString(matriz.matriz));
+		 */
 	}
 }
