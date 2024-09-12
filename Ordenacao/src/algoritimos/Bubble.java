65445a4aa71cfabbeb4dd6eff51f3019d3baa1eb
Aijalon Júnior <juniorsjs6@hotmail.com>
2024-09-11 22:04:27 -0300

    Insertion

--- Ordenacao/src/algoritimos/Bubble.java
+++ Ordenacao/src/algoritimos/Bubble.java
@@ -1,4 +1,4 @@
-package bubble;
+package algoritimos;
 
 import java.util.LinkedList;
 
--- Ordenacao/src/algoritimos/Inicio.java
+++ Ordenacao/src/algoritimos/Inicio.java
@@ -1,32 +1,44 @@
-package bubble;
+package algoritimos;
 
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class Inicio {
 	public static void main(String[] args) {
 		Bubble bubble = new Bubble();
-		int[] vetor = new int[100000];
+		Insertion insertion = new Insertion();
+		int[] vetor = {10, 2, 1, 0, 5};
 		LinkedList<Integer> lista = new LinkedList<Integer>();
+		Queue<Integer> fila = new LinkedList<Integer>();
+		fila.add(10);
+		fila.add(2);
+		fila.add(1);
+		fila.add(0);
+		fila.add(5);
+		
+		
 		
 	//	for (int i = 0; i < vetor.length; i++) {
 	//		vetor[i] = (int) (Math.random()*vetor.length);
 	//	}
-		for (int i = 0; i < 10; i++) {
-			lista.add(i, (int) (Math.random()*10));
-		}
+	//	for (int i = 0; i < 10; i++) {
+			//lista.add(i, (int) (Math.random()*10));
+		//}
 		
 		//System.out.println((int) (Math.random()*1000));
 		
 		System.out.println("Vetor desordenado.");
-		//System.out.println(Arrays.toString(vetor));	
-		
+		for (int i = 0; i < fila.size(); i++) {
+			System.out.println(fila.remove());
+		}
+
 		
 		System.out.println("Vetor ordenado.");
-		long inicio = System.currentTimeMillis();
+	/*	long inicio = System.currentTimeMillis();)
 		System.out.println(bubble.bubbleSort(lista).toString());
 		long fim = System.currentTimeMillis();
-		System.out.println("Tempo: "+(fim-inicio));
+		System.out.println("Tempo: "+(fim-inicio));*/
 	}
 	
 	
