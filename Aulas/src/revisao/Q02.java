89e9fd22edd61445821985757db6e2c22916b20b
Aijalon Júnior <juniorsjs6@hotmail.com>
2025-02-27 20:29:52 -0300

    Update Q02.java

--- Aulas/src/revisao/Q02.java
+++ Aulas/src/revisao/Q02.java
@@ -2,4 +2,37 @@ package revisao;
 
 public class Q02 {
 
+	public static void main(String[] args) {
+		int[] vetor = new int[5];
+		
+		int[][] matriz = new int[5][5];
+		
+		//preencher vetor
+		for (int i = 0; i < vetor.length; i++) {
+			vetor[i] = i+1;
+		}
+		
+		//preencher matriz
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz.length; j++) {
+				matriz[i][j] = (i*1)+(j+1);
+			}
+		}
+		
+		System.out.print("Vetor: ");
+		for (int i = 0; i < vetor.length; i++) {
+
+		}
+		
+
+		System.out.print("\nMatriz: \n");
+		
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz.length; j++) {
+				
+				
+			}
+		}
+		
+	}
 }
