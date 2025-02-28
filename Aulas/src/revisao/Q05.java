93475e93489a8d3472cdfa7d5aed29d448e02d6f
Aijalon Júnior <juniorsjs6@hotmail.com>
2025-02-27 21:56:59 -0300

    Revisão 2

--- Aulas/src/revisao/Q05.java
+++ Aulas/src/revisao/Q05.java
@@ -1,9 +1,9 @@
 package revisao;
 
-public class Q02 {
+public class Q05 {
 
 	public static void main(String[] args) {
-		int[] vetor = new int[5];
+int[] vetor = new int[5];
 		
 		int[][] matriz = new int[5][5];
 		
@@ -20,22 +20,25 @@ public class Q02 {
 		}
 		
 		System.out.print("Vetor: ");
+		int soma = 0;
 		for (int i = 0; i < vetor.length; i++) {
-			if(vetor[i]>3) {
-				System.out.print(vetor[i]+" ");
+			if(vetor[i]%2==1) {
+				soma= soma + vetor[i];				
 			}
 		}
 		
-
+		System.out.print(soma);
 		System.out.print("\nMatriz: \n");
-		
+		soma=0;
 		for (int i = 0; i < matriz.length; i++) {
 			for (int j = 0; j < matriz.length; j++) {
-				if(matriz[i][j]>3) {
-					System.out.print(matriz[i][j]+" ");
+				if(matriz[i][j]%2==1) {
+					soma = soma+matriz[i][j];					
 				}
 			}
 		}
 		
+		System.out.print(soma);
+
 	}
 }
